016c2923cff4a5e77dfcea25c146fdf867833440
Belinschi Dima <belinski.d@gmail.com>
2016-08-08 20:26:47 +0300

    add follow/unfollow methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,19 @@ class User < ApplicationRecord
   has_many :active_relationships, class_name:  "Relationship",
                                   foreign_key: "follower_id",
                                   dependent:   :destroy
+  has_many :following, through: :active_relationships, source: :followed
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  # Follows a user.
+ def follow(other_user)
+   active_relationships.create(followed_id: other_user.id)
+ end
+
+ # Unfollows a user.
+ def unfollow(other_user)
+   active_relationships.find_by(followed_id: other_user.id).destroy
+ end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  it { should have_many :tweets }
-  it { should have_many :active_relationships }
+  it { should have_many(:tweets).dependent(:destroy) }
+  it { should have_many(:active_relationships).class_name('Relationship').with_foreign_key("follower_id").dependent(:destroy) }
+  it { should have_many(:following).through(:active_relationships).source(:followed) }
+
+  describe ".follow" do
+    let!(:follower) { create(:user) }
+    let!(:followed) { create(:user) }
+    it "Create active_relationship" do
+      expect{ follower.follow(followed) }.to change{ follower.reload.active_relationships.count }.by(1)
+    end
+  end
+
+  describe ".follow" do
+    let!(:follower) { create(:user) }
+    let!(:followed) { create(:user) }
+    it "Destroy active_relationship" do
+      follower.follow(followed)
+      expect{ follower.unfollow(followed) }.to change{ follower.reload.active_relationships.count }.by(-1)
+    end
+  end
 end
