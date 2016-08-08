d8899400bbad185349ad13ba5a5f25e74663033c
Belinschi Dima <belinski.d@gmail.com>
2016-08-08 21:30:58 +0300

    erb2slim

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,6 @@ Rails.application.routes.draw do
       get :following, :followers
     end
   end
+  resources :tweets,  only: [:create, :destroy]
+  resources :relationships, only: [:create, :destroy]
 end
