26b6db34010cab9b6503132ed124cf9142682080
Belinschi Dima <belinski.d@gmail.com>
2016-08-09 17:49:05 +0300

    sign_in with username or email

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,6 +18,15 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  def self.find_for_database_authentication(warden_conditions)
+    conditions = warden_conditions.dup
+    if login = conditions.delete(:login)
+      where(conditions.to_h).where(["lower(username) = :value OR lower(email) = :value", { value: login.downcase }]).first
+    elsif conditions.key?(:username) || conditions.key?(:email)
+      where(conditions.to_h).first
+    end
+  end
+  
   # Follows a user.
   def follow(other_user)
     active_relationships.create(followed_id: other_user.id)
