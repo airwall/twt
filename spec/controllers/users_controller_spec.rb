72d917d69b9bd65fc9aefce5160ddb85049c9e5b
Belinschi Dima <belinski.d@gmail.com>
2016-08-09 23:53:26 +0300

    add test to followers & following action UsersCntroller

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -3,10 +3,13 @@ require 'rails_helper'
 RSpec.describe UsersController, type: :controller do
   sign_in_user
   let(:users) { create_list(:user, 4) }
+  let(:tweets) { create_list(:tweet, 2, user_id: @user.id) }
+  let(:following) { create(:user) }
+  let(:followers) { create(:user) }
 
   describe "/GET index" do
     before { get :index }
-    it "populates an array all tweets" do
+    it "populates an array all users" do
       expect(assigns(:users)).to match_array(users << @user)
     end
 
@@ -15,4 +18,40 @@ RSpec.describe UsersController, type: :controller do
     end
   end
 
+  describe "/GET show" do
+    context "current_user page" do
+      before { get :show, params: { id: @user.id } }
+      it "populates an array all tweets" do
+        expect(assigns(:tweets)).to match_array(tweets)
+      end
+
+      it "render show view" do
+        expect(response).to render_template :show
+      end
+    end
+  end
+
+  describe "/GET following" do
+    before { get :following, params: { id: @user } }
+    it "populates an array followers page" do
+      @user.follow(following)
+      expect(assigns(:following)).to match_array(following)
+    end
+
+    it "render user view" do
+      expect(response).to render_template :following
+    end
+  end
+
+  describe "/GET followers" do
+    before { get :followers, params: { id: @user } }
+    it "populates an array followers page" do
+      followers.follow(@user)
+      expect(assigns(:followers)).to match_array(followers)
+    end
+
+    it "render user view" do
+      expect(response).to render_template :followers
+    end
+  end
 end
