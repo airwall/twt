e27d5e5a76a519a3ea876688a20e6117e8c0e1b4
Belinschi Dima <belinski.d@gmail.com>
2016-08-09 18:13:58 +0300

    add home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,6 @@
 class HomeController < ApplicationController
-
+  before_action :authenticate_user!
+  
   def index
     @tweets = Tweet.where("user_id in (?) OR user_id = ?", current_user.following_ids, current_user).order('created_at DESC')
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,20 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  root 'static_pages#index'
-  get 'home' => 'home#index'
+  authenticated :user do
+     root to: "home#index", as: "home"
+   end
+
+   unauthenticated :user do
+     root 'static_pages#index'
+   end
+
   resources :users do
     member do
       get :following, :followers
     end
   end
+  
   resources :tweets,  only: [:create, :destroy]
   resources :relationships, only: [:create, :destroy]
 end
