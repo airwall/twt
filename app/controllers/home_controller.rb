ad3c4cf6ab2bd61d3e7eeeb8a737aa89c8d831d4
Belinschi Dima <belinski.d@gmail.com>
2016-08-09 22:18:27 +0300

    remove home controller

--- app/controllers/home_controller.rb
+++
@@ -1,7 +0,0 @@
-class HomeController < ApplicationController
-  before_action :authenticate_user!
-
-  def index
-    @tweets = Tweet.where("user_id in (?) OR user_id = ?", current_user.following_ids, current_user).order("created_at DESC")
-  end
-end
